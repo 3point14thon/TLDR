3b5eea7bcdaef4511191b17e450db62632397b93
3point14thon <chris.alan.bowers@gmail.com>
2018-11-17 14:53:39 -0800

    proper documentation

--- src/collect_paragraphs.py
+++ src/collect_paragraphs.py
@@ -3,6 +3,15 @@ from bs4.element import Comment
 import urllib.request
 
 def paragraphs_from_html(url):
+    '''
+    Returns the text tagged as paragraph from the specified url.
+
+    Args:
+        url: url to grab paragraph text from as a string.
+
+    Returns:
+        Returns the text tagged as paragraph from the specified url.
+    '''
     html = urllib.request.urlopen(url).read()
     soup = BeautifulSoup(html, 'html.parser')
     texts = soup.findAll('p')
