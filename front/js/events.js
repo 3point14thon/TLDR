3a20a1ea30d08f65a55fb3b336a3d0b1564b331f
Brendan Woodell <brendan.woodell@gmail.com>
2018-11-17 10:35:02 -0800

    put JS file in wrong spot and wasted 30 minutes

---
+++ front/js/events.js
@@ -0,0 +1,17 @@
+// document.addEventListener("DOMContentLoaded", function(docEvent){
+
+//     // const siteTable = document.getElementById('siteTable');
+//     // console.log('Here')
+
+//     // siteTable.addEventListener('mouseover', function(e){
+//     //     if (e.target !== e.currentTarget) {
+//     //         var clickedItem = e.target.id;
+//     //         console.log("Hello " + clickedItem);
+//     //     }
+//     //     e.stopPropagation();
+//     // })
+
+//     console.log('here')
+// })
+
+console.log('outer here')
