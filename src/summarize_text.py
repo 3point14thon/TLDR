15574df3e114e486211cb03d89da59b5c5c8e97c
3point14thon <chris.alan.bowers@gmail.com>
2018-11-17 14:07:48 -0800

    added default number of sentinces returned to 1

--- src/summarize_text.py
+++ src/summarize_text.py
@@ -2,7 +2,14 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from nltk import sent_tokenize
 import numpy as np
 
-def main(text, num_res):
+def summarizer(text, num_res=1):
+    '''
+    Outpust num_res amount of the most important sentinces in the input text
+    based on the sum of the tfidf vector for each sentince.
+
+    Args:
+
+    '''
     num_res = num_res * -1 -1
     sen = sent_tokenize(text)
     vectorizor = TfidfVectorizer(stop_words='english')
