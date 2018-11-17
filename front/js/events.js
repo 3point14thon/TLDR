2c1020f080b24df3c1ebd664030d1793951c2cc5
Brendan Woodell <brendan.woodell@gmail.com>
2018-11-17 10:43:55 -0800

    got mouseover event to fire mostly correctly

--- front/js/events.js
+++ front/js/events.js
@@ -1,17 +1,16 @@
-// document.addEventListener("DOMContentLoaded", function(docEvent){
+document.addEventListener("DOMContentLoaded", function(docEvent){
 
-//     // const siteTable = document.getElementById('siteTable');
-//     // console.log('Here')
+    const siteTable = document.getElementById('siteTable');
+    console.log('Here')
 
-//     // siteTable.addEventListener('mouseover', function(e){
-//     //     if (e.target !== e.currentTarget) {
-//     //         var clickedItem = e.target.id;
-//     //         console.log("Hello " + clickedItem);
-//     //     }
-//     //     e.stopPropagation();
-//     // })
-
-//     console.log('here')
-// })
-
-console.log('outer here')
+    siteTable.addEventListener('mouseover', function(e){
+        if (e.target !== e.currentTarget) {
+            if(e.target.classList.contains('top-matter')){
+                console.log('YAS QUEEN')
+            }
+            // var clickedItem = e.target.id;
+            // console.log(e.target.classList.value);
+        }
+        e.stopPropagation();
+    })
+})
