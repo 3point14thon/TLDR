d806298c6b80be6f62ee3303cc210bff2923c62d
Brendan Woodell <brendan.woodell@gmail.com>
2018-11-17 12:26:44 -0800

    crap, lost some stuff

--- front/js/events.js
+++ front/js/events.js
@@ -12,12 +12,12 @@ document.addEventListener("DOMContentLoaded", function(docEvent){
                 if(!tldr){
                     const p = document.createElement('p')
                     p.className = "TLDR"
-                    if(e.target.classList.contains('stickied')){
-                        p.innerText = "TLDR: This thread is dedicated to short questions and clarifications which don't require much discussion or those that can be given definitive answers may also be asked here."
-                    }
-                    else {
-                        p.innerText = 'TLDR: This thread represents something else. Very likely that XQC is bad and a toxic influence on your kids. But its still better than PUBG so...'
-                    }
+                    // if(e.target.classList.contains('stickied')){
+                    //     p.innerText = ""
+                    // }
+                    // else {
+                    //     p.innerText = 
+                    // }
                     tm.appendChild(p)
                 } 
             }
@@ -40,3 +40,12 @@ document.addEventListener("DOMContentLoaded", function(docEvent){
     })
 
 })
+
+const data = [{
+    Question: "TLDR: This thread is dedicated to short questions and clarifications which don't require much discussion or those that can be given definitive answers may also be asked here.",
+    Discussion: "TLDR: This thread represents something else. Very likely that XQC is bad and a toxic influence on your kids. But its still better than PUBG so...",
+    PSA: "TLDR: PUBLIC SERVICE ANNOUNCEMENT, Here we would put something very very very important. Maybe about Seagull, or Soe, or Monte. But definitely not anybody else. This is a briefing to let everyone know what we're about. Yay!",
+    Highlight: "TLDR: LOOK at this awesome thing a Pro did! Isn't it keeewwwwwl Kenny? Isn't it wholesome? Look at that Doomfist bringing doom upon others. Justice from orbit. Justice crashes to earth and crushes all those beneath it..... from above.",
+    Fluff: "TLDR: Seagull in a unicorn onsie. It's literally fluff. It's the fluffiest thing I have ever seen and it's wholesome and wonderful and great and I love it. LOVE IT WITH ME!",
+    Gossip: "TLDR: XQC XQC XQC XQC.... Dafran.... Fissure... XQC XQC XQC. Let's be honest ALL gossip is for XQC and I don't even see why we validate his child-like mannerisms. Like, ok yes he's the best Tank in the game HANDS DOWN. But... do we really want to glorify needing a new keyboard every week? He's a terrible influence and if I hear one more scream into the mic I will lose my damn mind."
+}]
